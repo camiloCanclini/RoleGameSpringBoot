3bd6f6e8dfd75a6256f43913f4d5b6d914e8e906
Camilo Canclini <camilocanclini@gmail.com>
2023-06-23 23:07:11 -0300

    first commit

---
+++ src/test/java/com/canclini/rolegame/RolegameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canclini.rolegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolegameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
